dfb611b5857295f0a1768405d1f1b1f4895e272f
Edvins Mineikis <edvinsmineikis@gmail.com>
2023-05-04 20:34:09 +0300

    create webapp layout

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
 import PinControl from './PinControl';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 
 const theme = createTheme({
   palette: {
@@ -26,14 +26,30 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <AppBar position="fixed" color="primary">
+        <AppBar position="relative" color="primary">
           <Toolbar>
             <Typography variant="h6" component="div">
-              ESP8266 NodeMCU Control Panel
+              ESP8266 NodeMCU GPIO Control Panel
             </Typography>
           </Toolbar>
         </AppBar>
-        <PinControl />
+        <Grid2 container spacing={1} style={{ marginTop: "16px"}}>
+          {[...Array(5)].map((e, i) => (
+            <Grid2 xs="auto">
+              <PinControl pinNumber={i} />
+            </Grid2>
+          ))}
+          {[...Array(2)].map((e, i) => (
+            <Grid2 xs="auto">
+              <PinControl pinNumber={i+9} />
+            </Grid2>
+          ))}
+          {[...Array(5)].map((e, i) => (
+            <Grid2 xs="auto">
+              <PinControl pinNumber={i+12} />
+            </Grid2>
+          ))}
+        </Grid2>
       </div>
     </ThemeProvider>
   );
--- webapp/src/PinControl.js
+++ webapp/src/PinControl.js
@@ -1,9 +1,59 @@
+import * as React from 'react';
 import { Typography } from "@mui/material";
+import Card from '@mui/material/Card';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import Stack from '@mui/material/Stack';
 
-export default function PinControl() {
+
+export default function PinControl({ pinNumber }) {
+    const [value, setValue] = React.useState('');
+    const handleChange = event => {
+        const result = event.target.value.replace(/\D/g, '');
+        setValue(event.target.value);
+    };
+
     return (
-        <Typography variant="h6" component="div">
-            Hello
-        </Typography>
+        <Card>
+            <Stack spacing={2}>
+                <Typography variant="h6">{"GPIO " + pinNumber}</Typography>
+                <Grid2 container>
+                    <Grid2 xs={9} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <FormControl sx={{ m: 1, minWidth: 120 }}>
+                            <InputLabel id="pin-action" fullwidth>Command</InputLabel>
+                            <Stack spacing={2}>
+                                <Select
+                                    labelId="pin-action"
+                                    id="pin-action"
+                                    label="Command"
+                                    fullwidth
+                                >
+                                    <MenuItem value="pinMode">pinMode</MenuItem>
+                                    <MenuItem value="digitalRead">digitalRead</MenuItem>
+                                    <MenuItem value="analogRead">analogRead</MenuItem>
+                                    <MenuItem value="digitalWrite">digitalWrite</MenuItem>
+                                    <MenuItem value="analogWrite">analogWrite</MenuItem>
+                                </Select>
+                                <TextField
+                                    id="pin-value"
+                                    label="Value"
+                                    type="number"
+                                    onChange={handleChange}
+                                    value={value}
+                                />
+                            </Stack>
+                        </FormControl>
+                    </Grid2>
+                    <Grid2 xs={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Button variant="contained" color="secondary" style={{ maxWidth: '60px', maxHeight: "120px", minWidth: '60px', minHeight: "120px" }}>POST</Button>
+                    </Grid2>
+                </Grid2>
+            </Stack>
+        </Card >
     );
 }
